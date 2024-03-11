6f7d398c1f2eea3cff542367b2c36fb736fe7000
fadelyking <fadely195@gmail.com>
2024-03-11 16:13:30 +0300

    get all ids of a certain page

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const post_id = "53582174";
+const jobsPage = `https://api.scraperapi.com/?api_key=0c3668c271e0cedf9b2a18de9114b0dc&url=https://www.expatriates.com/classifieds/bahrain/jobs&follow_redirect=false&device_type=desktop&country_code=eu&render=true`;
+const url = `https://api.scraperapi.com/?api_key=0c3668c271e0cedf9b2a18de9114b0dc&url=https%3A%2F%2Fwww.expatriates.com%2Fcls%2F${post_id}.html&follow_redirect=false&device_type=desktop&country_code=eu&render=true`;
 
+function log(value) {
+    console.log(value);
+}
+
+const fetchData = async () => {
+    try {
+        let res = await axios.get(jobsPage);
+        let $ = await cheerio.load(res.data);
+        const text = $("li onclick").text().trim();
+        const ids = $("li[onclick]")
+            .map((i, elem) => {
+                const onclickValue = $(elem).attr("onclick");
+                const match = onclickValue.match(/\/cls\/(\d+)\.html/);
+                return match ? match[1] : null;
+            })
+            .get();
+    } catch (e) {
+        log(e);
+    }
+};
+fetchData();
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
